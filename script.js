f4da3b944594b672890d58df01b2f178a934a2fe
megarard <adriyantomuhammadegar@gmail.com>
2026-03-03 11:38:54 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// Pesan saat website dibuka
+window.onload = function() {
+  console.log("IlmuNusa berhasil dijalankan 🚀");
+};
+
+// Fungsi tombol Mulai Belajar
+function mulaiBelajar() {
+  alert("Semangat belajar di IlmuNusa! 🎓🔥");
+}
+
+// Fungsi login sederhana
+function loginBerhasil() {
+  alert("Login berhasil! Selamat datang di IlmuNusa 🎉");
+}
